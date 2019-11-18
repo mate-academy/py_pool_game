74098bff2f430ab4afab7204cf25dd65edea7885
Andrii <andrewsotnikov88@gmail.com>
2019-11-18 23:09:59 +0200

    Fixed styles according PEP-8

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-
+"""This is docstring"""
 import pool
 import fishes
 
 import config
 
-if __name__ == '__main__':
-    p = pool.Pool()
-    p.fill(fishes.Predator, int(config.config['App']['Predators']))
-    p.fill(fishes.Victim, int(config.config['App']['Victims']))
+if __name__ == "__main__":
+    P = pool.Pool()
+    P.fill(fishes.Predator, int(config.CONFIG["App"]["Predators"]))
+    P.fill(fishes.Victim, int(config.CONFIG["App"]["Victims"]))
     for i in range(config.get_ticks()):
-        print(p)
-        p.tick()
+        print(P)
+        P.tick()
--- pool.py
+++ pool.py
@@ -1,60 +1,69 @@
-import os
+"""This is docstring"""
 import random
-
+import math
 import config
 
-import math
-
-POOL_WIDTH = int(config.config['Pool']['Width'])
-POOL_HEIGHT = int(config.config['Pool']['Height'])
+POOL_WIDTH = int(config.CONFIG["Pool"]["Width"])
+POOL_HEIGHT = int(config.CONFIG["Pool"]["Height"])
 
 
 class Pool:
+    """This is class docstring"""
+
     def __init__(self):
-        self._width = POOL_WIDTH; self._height = POOL_HEIGHT
-        self._fishes = []
+        self._width = POOL_WIDTH
+        self._height = POOL_HEIGHT
+        self.fishes = []
 
-    def get_size(pool):
-        return pool._width, pool._height
+    def get_size(self):
+        """This is module docstring"""
+        return self._width, self._height
 
-    def fill(self, fish_type:int, number:int):
-        self._fishes += [fish_type(
+    def fill(self, fish_type, number: int):
+        """This is function docstring"""
+        self.fishes += [fish_type(
             random.randint(0, self._width - 1),
             random.randint(0, self._height - 1)
         ) for _ in range(number)]
 
     def __str__(self):
-        s = '+' + '-'*self._width + '+\n'
-        p = [[' ']*self._width for _ in range(self._height)]
-        for fish in self._fishes:
-            p[fish.get_pos()[0]][fish.get_pos()[1]] = repr(fish)
-        for row in p:
-            s += '|' + ''.join(row) + '|\n'
-        s += '+' + '-'*self._width + '+\n'
-        return s
+        value = "+" + "-" * self._width + "+\n"
+        p_value = [[" "] * self._width for _ in range(self._height)]
+        for fish in self.fishes:
+            p_value[fish.get_pos()[0]][fish.get_pos()[1]] = repr(fish)
+        for row in p_value:
+            value += "|" + "".join(row) + "|\n"
+        value += "+" + "-" * self._width + "+\n"
+        return value
 
     def tick(self):
-        for fish in self._fishes:
+        """This is function docstring"""
+        for fish in self.fishes:
             fish.move(self)
             fish.eat(self)
 
-        for fish in self._fishes.copy():
+        for fish in self.fishes.copy():
             if not fish.is_alive():
                 self.kill(fish)
-        else:
-            for fish in self._fishes.copy():
-                fish.born(self)
+        for fish in self.fishes.copy():
+            fish.born(self)
 
-    def get_nearest_victim(self, x, y):
-        nearest_victims = [fish.get_pos() for fish in self._fishes if fish.is_victim()]
+    def get_nearest_victim(self, x_val, y_val):
+        """This is function docstring"""
+        nearest_victims = [fish.get_pos() for fish in self.fishes
+                           if fish.is_victim]
         if not nearest_victims:
             return (0, 0)
-        return tuple(min(nearest_victims, key=lambda f: math.hypot(f[0] - x, f[1] - y)))
+        return tuple(min(nearest_victims,
+                         key=lambda f: math.hypot(f[0] - x_val, f[1] - y_val)))
 
     def get_victim(self, pos):
-        return [fish for fish in self._fishes if fish.get_pos() == pos and fish.is_victim()]
+        """This is function docstring"""
+        return [
+            fish for fish in self.fishes if
+            fish.get_pos() == pos and fish.is_victim
+        ]
 
     def kill(self, victim):
-        self._fishes.remove(victim)
-
-
+        """This is function docstring"""
+        self.fishes.remove(victim)
