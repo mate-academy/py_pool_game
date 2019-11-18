d557209923c979fabc2b5319ba3074da291b3568
logotip <logotip123@yahoo.com>
2019-11-18 19:36:20 +0200

    My tests

--- fishes.py
+++ fishes.py
@@ -1,6 +1,8 @@
+"""
+About fishes
+"""
 import random
 import json
-import random
 
 import pool
 
@@ -8,6 +10,9 @@ X, Y = 0, 1
 
 
 class Fish:
+    """
+    class fish
+    """
     def __init__(self, x, y):
         self._pos = [x, y]
         self._life_counter = 10
@@ -15,41 +20,56 @@ class Fish:
         self._born_num = 0
         self._is_not_hungry = 0
 
+    is_victim = False
+
     def get_pos(self):
+        """
+            get fish position
+        """
         return self._pos
 
-    def move(self, p: pool.Pool) -> tuple:
+    def move(self, how):
+        """
+            fish moving
+        """
         self._life_counter -= 1
-        self._move(p)
-        self.place_in_bounds(p)
+        self._move(how)
+        self.place_in_bounds(how)
 
     def is_alive(self):
+        """
+            are fish alive?
+        """
         return self._life_counter > 0
 
-    def _move(self, p: pool.Pool):
+    def _move(self, how: pool.Pool):
         pass
 
-    def is_victim(self) -> bool:
-        return False
-
-    def place_in_bounds(self, p: list):
+    def place_in_bounds(self, place):
+        """
+            place in bounds
+        """
         try:
-            self._pos[X] = min(max(self._pos[X], 0), p.get_size()[X] - 1)
-            self._pos[Y] = min(max(self._pos[Y], 0), p.get_size()[Y] - 1)
-        except Exception:
-            pass
+            self._pos[X] = min(max(self._pos[X], 0), place.get_size()[X] - 1)
+            self._pos[Y] = min(max(self._pos[Y], 0), place.get_size()[Y] - 1)
         except ValueError:
             print("Oooops!")
 
-    def eat(self, p: pool.Pool):
-        pass
+    def eat(self, pooler: pool.Pool):
+        """DOCSTRING"""
 
-    def born(self, p: pool.Pool):
+    def born(self, eat: pool.Pool):
+        """
+            born
+        """
         if random.randint(1, 10) < self._born_rate:
-            p.fill(self.__class__, self._born_num)
+            eat.fill(self.__class__, self._born_num)
 
 
 class Predator(Fish):
+    """
+        class predator
+    """
     with open("predator.json", 'rt') as f:
         state = json.load(f)
 
@@ -60,20 +80,21 @@ class Predator(Fish):
         super().__init__(x, y)
         self.__dict__.update(self.state)
 
-    def _move(self, p: pool.Pool=[]):
+    def _move(self, how):
         self._is_not_hungry -= 1
-        victim = p.get_nearest_victim(*self._pos)
-        self._pos[X]+= 2 if victim[X]>self._pos[X] else -2
-        self._pos[Y]+= 2 if victim[Y]>self._pos[Y] else -2
+        victim = how.get_nearest_victim(*self._pos)
+        self._pos[X] += 2 if victim[X] > self._pos[X] else -2
+        self._pos[Y] += 2 if victim[Y] > self._pos[Y] else -2
 
-    def eat(self, p: pool.Pool) -> int:
+    def eat(self, pooler: pool.Pool):
         """
+        eat
         """
-        victims = p.get_victim(self.get_pos())
+        victims = pooler.get_victim(self.get_pos())
         if victims:
             self._is_not_hungry += 3
             for victim in victims:
-                p.kill(victim)
+                pooler.kill(victim)
 
     def __repr__(self):
         return "P"
@@ -83,6 +104,7 @@ class Predator(Fish):
 
 
 class Victim(Fish):
+    """VICTIM"""
     with open("victim.json", 'rt') as f:
         state = json.load(f)
 
@@ -90,11 +112,23 @@ class Victim(Fish):
         super().__init__(x, y)
         self.__dict__.update(self.state)
 
-    def _move(self, p: pool.Pool):
-        self._pos[X] += random.randint(-1,1)
-        self._pos[Y] += random.randint(-1,1)
+    is_victim = True
+
+    def _move(self, how):
+        self._pos[X] += random.randint(-1, 1)
+        self._pos[Y] += random.randint(-1, 1)
 
     def __repr__(self):
         return "V"
 
-    is_victim = lambda self: True
+
+class Pike(Predator):
+    """
+    class Pike
+    """
+    with open("pike.json", 'rt') as f:
+        state = json.load(f)
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.__dict__.update(self.state)
--- test_pool.py
+++ test_pool.py
@@ -1,96 +1,117 @@
-import unittest
+"""DOCSTRING"""
 import unittest.mock
 import pool
 import fishes
 
 
 class TestPool(unittest.TestCase):
+    """DOCSTRING"""
     def setUp(self) -> None:
-        self.p = pool.Pool()
+        self.pool123 = pool.Pool()
 
     def test_pool(self):
-        self.assertEqual(self.p.get_size(), (10, 10), "Pool size doesn't equal 10x10")
+        """DOCSTRING"""
+        self.assertEqual(self.pool123.get_size(), (20, 20),
+                         "Pool size doesn't equal 20x20")
 
     def test_fill_predators(self):
-        PREDATORS_QUANTITY = 3
-        l = len(self.p._fishes)
-        self.p.fill(fishes.Predator, PREDATORS_QUANTITY)
-        self.assertEqual(len(self.p._fishes), l + PREDATORS_QUANTITY)
+        """DOCSTRING"""
+        predators_qual = 3
+        lenght = len(self.pool123.fishes)
+        self.pool123.fill(fishes.Predator, predators_qual)
+        self.assertEqual(len(self.pool123.fishes), lenght + predators_qual)
 
     @unittest.mock.patch('random.randint')
-    def test_fill_predators_random(self, m):
-        m.side_effect = 0, 1
-        self.p.fill(fishes.Predator, 1)
-        self.assertEqual(self.p._fishes[0].get_pos(), [0, 1])
+    def test_fill_predators_random(self, make):
+        """DOCSTRING"""
+        make.side_effect = 0, 1
+        self.pool123.fill(fishes.Predator, 1)
+        self.assertEqual(self.pool123.fishes[0].get_pos(), [0, 1])
 
     @unittest.mock.patch('random.randint')
-    def test_nearest_victim(self, m):
-        m.side_effect = 1, 1, 5, 5, 9, 0
-        self.p.fill(fishes.Victim, 3)
-        self.assertEqual(self.p.get_nearest_victim(2, 2), (1, 1))
-        self.assertEqual(self.p.get_nearest_victim(8, 1), (9, 0))
+    def test_nearest_victim(self, maker):
+        """DOCSTRING"""
+        maker.side_effect = 1, 1, 5, 5, 9, 0
+        self.pool123.fill(fishes.Victim, 3)
+        self.assertEqual(self.pool123.get_nearest_victim(2, 2), (1, 1))
+        self.assertEqual(self.pool123.get_nearest_victim(8, 1), (9, 0))
 
     @unittest.mock.patch('random.randint')
-    def test_get_victim(self, m):
-        m.side_effect = 1, 1
-        self.p.fill(fishes.Victim, 1)
-        self.assertEqual(self.p.get_victim([1, 1]), [self.p._fishes[0]])
+    def test_get_victim(self, make):
+        """DOCSTRING"""
+        make.side_effect = 1, 1
+        self.pool123.fill(fishes.Victim, 1)
+        self.assertEqual(self.pool123.get_victim([1, 1]),
+                         [self.pool123.fishes[0]])
 
     def tearDown(self) -> None:
         pass
 
 
 class TestFish(unittest.TestCase):
+    """DOCSTRING"""
     def test_fish_is_in_bounds1(self):
-        vc = fishes.Fish(-1, -1)
-        vc.place_in_bounds(pool.Pool())
-        self.assertEqual(vc.get_pos(), [0, 0])
+        """DOCSTRING"""
+        vconat = fishes.Fish(-1, -1)
+        vconat.place_in_bounds(pool.Pool())
+        self.assertEqual(vconat.get_pos(), [0, 0])
 
     def test_fish_is_in_bounds2(self):
-        vc = fishes.Fish(10, 10)
-        vc.place_in_bounds(pool.Pool())
-        self.assertEqual(vc.get_pos(), [9, 9])
+        """DOCSTRING"""
+        vconat = fishes.Fish(20, 20)
+        vconat.place_in_bounds(pool.Pool())
+        self.assertEqual(vconat.get_pos(), [19, 19])
 
 
 class TestPredator(unittest.TestCase):
+    """DOCSTRING"""
     def test_predator(self):
-        pr = fishes.Predator(2, 3)
-        self.assertEqual(repr(pr), 'P')
+        """DOCSTRING"""
+        prokal = fishes.Predator(2, 3)
+        self.assertEqual(repr(prokal), 'P')
 
     def test_predator_pos(self):
-        pr = fishes.Predator(2, 3)
-        self.assertEqual(pr.get_pos(), [2, 3])
+        """DOCSTRING"""
+        prokal = fishes.Predator(2, 3)
+        self.assertEqual(prokal.get_pos(), [2, 3])
 
     @unittest.mock.patch('random.randint')
-    def test_predator_move(self, m):
-        p = pool.Pool()
-        m.side_effect = 1, 1, 5, 5
-        p.fill(fishes.Victim, 1)
-        p.fill(fishes.Predator, 1)
-        p._fishes[1].move(p)
-        self.assertEqual(p._fishes[1].get_pos(), [3, 3])
+    def test_predator_move(self, make):
+        """DOCSTRING"""
+        porka = pool.Pool()
+        make.side_effect = 1, 1, 5, 5
+        porka.fill(fishes.Victim, 1)
+        porka.fill(fishes.Predator, 1)
+        porka.fishes[1].move(porka)
+        self.assertEqual(porka.fishes[1].get_pos(), [3, 3])
 
     @unittest.mock.patch('random.randint')
-    def test_predator_eating(self, m):
-        p = pool.Pool()
-        m.side_effect = 1, 1, 1, 1
-        p.fill(fishes.Predator, 1)
-        p.fill(fishes.Victim, 1)
-        p._fishes[0].eat(p)
-        self.assertEqual(len(p._fishes), 1)
+    def test_predator_eating(self, make):
+        """DOCSTRING"""
+        porka = pool.Pool()
+        make.side_effect = 1, 1, 1, 1
+        porka.fill(fishes.Predator, 1)
+        porka.fill(fishes.Victim, 1)
+        porka.fishes[0].eat(porka)
+        self.assertEqual(len(porka.fishes), 1)
+
 
 class TestVictim(unittest.TestCase):
+    """DOCSTRING"""
     def test_victim(self):
-        vc = fishes.Victim(2, 3)
-        self.assertEqual(repr(vc), 'V')
+        """DOCSTRING"""
+        vcolka = fishes.Victim(2, 3)
+        self.assertEqual(repr(vcolka), 'V')
 
     def test_victim_pos(self):
-        vc = fishes.Victim(2, 3)
-        self.assertEqual(vc.get_pos(), [2, 3])
+        """DOCSTRING"""
+        vcolka = fishes.Victim(2, 3)
+        self.assertEqual(vcolka.get_pos(), [2, 3])
 
     @unittest.mock.patch('random.randint')
-    def test_victim_correct_move(self, m):
-        vc = fishes.Victim(2, 3)
-        m.side_effect = 1, 1
-        vc.move(pool.Pool())
-        self.assertEqual(vc.get_pos(), [3, 4])
+    def test_victim_correct_move(self, make):
+        """DOCSTRING"""
+        vcontra = fishes.Victim(2, 3)
+        make.side_effect = 1, 1
+        vcontra.move(pool.Pool())
+        self.assertEqual(vcontra.get_pos(), [3, 4])
