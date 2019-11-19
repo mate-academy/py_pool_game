d6de11db74669e655d5eb3d91c686325425f11b0
Oleh_M <morhaliuk@gmail.com>
2019-11-20 01:01:26 +0200

    Corrected

--- fishes.py
+++ fishes.py
@@ -1,4 +1,4 @@
-import random
+"""Module fishes"""
 import json
 import random
 
@@ -8,6 +8,10 @@ X, Y = 0, 1
 
 
 class Fish:
+    """Fish class"""
+
+    is_victim = False
+
     def __init__(self, x, y):
         self._pos = [x, y]
         self._life_counter = 10
@@ -16,40 +20,67 @@ class Fish:
         self._is_not_hungry = 0
 
     def get_pos(self):
+        """
+
+        :return:
+        """
         return self._pos
 
-    def move(self, p: pool.Pool) -> tuple:
+    def move(self, pol):
+        """
+
+        :param pol:
+        :return:
+        """
         self._life_counter -= 1
-        self._move(p)
-        self.place_in_bounds(p)
+        self._move(pol)
+        self.place_in_bounds(pol)
 
     def is_alive(self):
+        """
+
+        :return:
+        """
         return self._life_counter > 0
 
-    def _move(self, p: pool.Pool):
-        pass
+    def _move(self, pol: pool.Pool):
+        """
 
-    def is_victim(self) -> bool:
-        return False
+        :param pol:
+        :return:
+        """
 
-    def place_in_bounds(self, p: list):
+    def place_in_bounds(self, pol):
+        """
+
+        :param pol:
+        :return:
+        """
         try:
-            self._pos[X] = min(max(self._pos[X], 0), p.get_size()[X] - 1)
-            self._pos[Y] = min(max(self._pos[Y], 0), p.get_size()[Y] - 1)
-        except Exception:
-            pass
+            self._pos[X] = min(max(self._pos[X], 0), pol.get_size()[X] - 1)
+            self._pos[Y] = min(max(self._pos[Y], 0), pol.get_size()[Y] - 1)
         except ValueError:
             print("Oooops!")
 
-    def eat(self, p: pool.Pool):
-        pass
+    def eat(self, pol: pool.Pool):
+        """
 
-    def born(self, p: pool.Pool):
+        :param pol:
+        :return:
+        """
+
+    def born(self, pol: pool.Pool):
+        """
+
+        :param pol:
+        :return:
+        """
         if random.randint(1, 10) < self._born_rate:
-            p.fill(self.__class__, self._born_num)
+            pol.fill(self.__class__, self._born_num)
 
 
 class Predator(Fish):
+    """Predator class"""
     with open("predator.json", 'rt') as f:
         state = json.load(f)
 
@@ -60,29 +91,55 @@ class Predator(Fish):
         super().__init__(x, y)
         self.__dict__.update(self.state)
 
-    def _move(self, p: pool.Pool=[]):
-        self._is_not_hungry -= 1
-        victim = p.get_nearest_victim(*self._pos)
-        self._pos[X]+= 2 if victim[X]>self._pos[X] else -2
-        self._pos[Y]+= 2 if victim[Y]>self._pos[Y] else -2
+    def _move(self, pol=None):
+        """
 
-    def eat(self, p: pool.Pool) -> int:
+        :param pol:
+        :return:
         """
+        self._is_not_hungry -= 1
+        victim = pol.get_nearest_victim(*self._pos)
+        self._pos[X] += 2 if victim[X] > self._pos[X] else -2
+        self._pos[Y] += 2 if victim[Y] > self._pos[Y] else -2
+
+    def eat(self, pol: pool.Pool):
         """
-        victims = p.get_victim(self.get_pos())
+
+        :param pol:
+        :return:
+        """
+        victims = pol.get_victim(self.get_pos())
         if victims:
             self._is_not_hungry += 3
             for victim in victims:
-                p.kill(victim)
+                pol.kill(victim)
 
     def __repr__(self):
         return "P"
 
     def is_alive(self):
+        """
+
+        :return:
+        """
         return super().is_alive() and self._is_not_hungry > 0
 
 
+class Pike(Predator):
+    """Pike class"""
+    with open("pike.json", 'rt') as f:
+        state = json.load(f)
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.__dict__.update(self.state)
+
+
 class Victim(Fish):
+    """Victim class"""
+
+    is_victim = True
+
     with open("victim.json", 'rt') as f:
         state = json.load(f)
 
@@ -90,11 +147,14 @@ class Victim(Fish):
         super().__init__(x, y)
         self.__dict__.update(self.state)
 
-    def _move(self, p: pool.Pool):
-        self._pos[X] += random.randint(-1,1)
-        self._pos[Y] += random.randint(-1,1)
+    def _move(self, pol: pool.Pool):
+        """
+
+        :param pol:
+        :return:
+        """
+        self._pos[X] += random.randint(-1, 1)
+        self._pos[Y] += random.randint(-1, 1)
 
     def __repr__(self):
         return "V"
-
-    is_victim = lambda self: True
--- pool.py
+++ pool.py
@@ -1,60 +1,92 @@
-import os
+"""Module Pool"""
 import random
+import math
 
 import config
 
-import math
-
-POOL_WIDTH = int(config.config['Pool']['Width'])
-POOL_HEIGHT = int(config.config['Pool']['Height'])
+POOL_WIDTH = int(config.CONFIG['Pool']['Width'])
+POOL_HEIGHT = int(config.CONFIG['Pool']['Height'])
 
 
 class Pool:
+    """Pool class"""
     def __init__(self):
-        self._width = POOL_WIDTH; self._height = POOL_HEIGHT
-        self._fishes = []
+        self._width = POOL_WIDTH
+        self._height = POOL_HEIGHT
+        self.fish_in_pool = []
 
-    def get_size(pool):
-        return pool._width, pool._height
+    def get_size(self):
+        """
 
-    def fill(self, fish_type:int, number:int):
-        self._fishes += [fish_type(
+        :return:
+        """
+        return self._width, self._height
+
+    def fill(self, fish_type, number: int):
+        """
+
+        :param fish_type:
+        :param number:
+        :return:
+        """
+        self.fish_in_pool += [fish_type(
             random.randint(0, self._width - 1),
             random.randint(0, self._height - 1)
         ) for _ in range(number)]
 
     def __str__(self):
-        s = '+' + '-'*self._width + '+\n'
-        p = [[' ']*self._width for _ in range(self._height)]
-        for fish in self._fishes:
-            p[fish.get_pos()[0]][fish.get_pos()[1]] = repr(fish)
-        for row in p:
-            s += '|' + ''.join(row) + '|\n'
-        s += '+' + '-'*self._width + '+\n'
-        return s
+        line = '+' + '-' * self._width + '+\n'
+        pol = [[' '] * self._width for _ in range(self._height)]
+        for fish in self.fish_in_pool:
+            pol[fish.get_pos()[0]][fish.get_pos()[1]] = repr(fish)
+        for row in pol:
+            line += '|' + ''.join(row) + '|\n'
+        line += '+' + '-' * self._width + '+\n'
+        return line
 
     def tick(self):
-        for fish in self._fishes:
+        """
+
+        :return:
+        """
+        for fish in self.fish_in_pool:
             fish.move(self)
             fish.eat(self)
 
-        for fish in self._fishes.copy():
+        for fish in self.fish_in_pool.copy():
             if not fish.is_alive():
                 self.kill(fish)
-        else:
-            for fish in self._fishes.copy():
-                fish.born(self)
 
-    def get_nearest_victim(self, x, y):
-        nearest_victims = [fish.get_pos() for fish in self._fishes if fish.is_victim()]
+        for fish in self.fish_in_pool.copy():
+            fish.born(self)
+
+    def get_nearest_victim(self, x_coord, y_coord):
+        """
+
+        :param x_coord:
+        :param y_coord:
+        :return:
+        """
+        nearest_victims = [fish.get_pos() for fish in self.fish_in_pool
+                           if fish.is_victim]
         if not nearest_victims:
             return (0, 0)
-        return tuple(min(nearest_victims, key=lambda f: math.hypot(f[0] - x, f[1] - y)))
+        return tuple(min(nearest_victims, key=lambda f:
+                         math.hypot(f[0] - x_coord, f[1] - y_coord)))
 
     def get_victim(self, pos):
-        return [fish for fish in self._fishes if fish.get_pos() == pos and fish.is_victim()]
+        """
+
+        :param pos:
+        :return:
+        """
+        return [fish for fish in self.fish_in_pool
+                if fish.get_pos() == pos and fish.is_victim]
 
     def kill(self, victim):
-        self._fishes.remove(victim)
+        """
 
-
+        :param victim:
+        :return:
+        """
+        self.fish_in_pool.remove(victim)
